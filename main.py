d8f7eb45c0dd5a630ae1f73ec95eaa8be4d34f39
Rohan1477 <105565487+Rohan1477@users.noreply.github.com>
2025-11-04 01:14:52 -0500

    testing main updates

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
-#test
+import tensorflow as tf
+import tensorflow_decision_forests as tfdf
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+train_file_path = "/.venv/house-prices-advanced-regression-techniques/train.csv"
+dataset_df = pd.read_csv(train_file_path)
+print("Full train dataset shape is {}".format(dataset_df.shape))
+
